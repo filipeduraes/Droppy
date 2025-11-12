a4fd9b08b3598a0fc82e3134d937e3f0c9919d47
Filipe Durães <fhdduraes@gmail.com>
2025-11-11 23:37:11 -0300

    Add script templates and create interaction system

---
+++ Assets/Scripts/InteractionSystem/InteractionAgent.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Droppy.InteractionSystem
+{
+    public class InteractionAgent : MonoBehaviour
+    {
+        private HashSet<IInteractable> currentInteractables = new();
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.TryGetComponent(out IInteractable interactable) && !currentInteractables.Contains(interactable))
+            {
+                interactable.EnterInteraction(this);
+                currentInteractables.Add(interactable);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.TryGetComponent(out IInteractable interactable) && !currentInteractables.Contains(interactable))
+            {
+                interactable.ExitInteraction(this);
+                currentInteractables.Remove(interactable);
+            }
+        }
+
+        public void StartInteraction()
+        {
+            foreach (IInteractable interactable in currentInteractables)
+            {
+                interactable.StartInteraction(this);
+            }
+        }
+
+        public void EndInteraction()
+        {
+            foreach (IInteractable interactable in currentInteractables)
+            {
+                interactable.EndInteraction(this);
+            }
+        }
+    }
+}
