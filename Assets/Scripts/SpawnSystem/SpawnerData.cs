b746d47b41e1efd3f8c327974384cf241e5a6c0a
hdpmatheus <186456475+anajulia-oliveyra@users.noreply.github.com>
2025-11-12 20:06:39 -0300

    Create SpawnSystem

---
+++ Assets/Scripts/SpawnSystem/SpawnerData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+    [CreateAssetMenu(menuName = "Droppy/SpawnerData", fileName = "SpawnerData")]
+    public class SpawnerData : ScriptableObject 
+    {
+    }
