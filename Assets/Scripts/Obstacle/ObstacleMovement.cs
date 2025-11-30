27d181eaeed5a96667aa7b8c48484a89e7598ed9
anajulia-oliveyra <anajulia.oliveyra@gmail.com>
2025-11-30 15:37:47 -0300

    Revert "Changes made"
    This reverts commit 073583599476eaadcfcbae3a7f1806895204d704.

--- Assets/Scripts/Obstacle/ObstacleMovement.cs
+++ Assets/Scripts/Obstacle/ObstacleMovement.cs
@@ -24,13 +24,8 @@ namespace Droppy.Obstacle
 
         private float markerY = 0.0f;
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
         private float currentSpeed;
         
-=======
-        private Collider2D obstacleCollider;
-
->>>>>>> Stashed changes
 =======
         private Collider2D obstacleCollider;
 
@@ -53,13 +48,10 @@ namespace Droppy.Obstacle
         private void OnEnable()
         {
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
             // Sorteia uma velocidade aleatória entre o Mínimo e o Máximo
             currentSpeed = Random.Range(minSpeed, maxSpeed);
             body.velocity = Vector2.up * currentSpeed;
 =======
-=======
->>>>>>> Stashed changes
             if (statModifierTrigger != null)
             {
                 statModifierTrigger.OnStatApplied += HitByPlayer;
@@ -81,14 +73,6 @@ namespace Droppy.Obstacle
             }
         }
 
-        private void OnDisable()
-        {
-            if (statModifierTrigger != null)
-            {
-                statModifierTrigger.OnStatApplied -= HitByPlayer;
-            }
-        }
-
         private void FixedUpdate()
         {
 <<<<<<< Updated upstream
@@ -99,9 +83,6 @@ namespace Droppy.Obstacle
             bool isAboveMarker = movementSpeed > 0.0f && body.position.y >= markerY;
             bool isBelowMarker = movementSpeed < 0.0f && body.position.y <= markerY;
 
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
 >>>>>>> Stashed changes
             if (isAboveMarker || isBelowMarker)
             {
