5d6b9f8e100965b5c5f845fc69008016e1f05c8c
Filipe Durães <fhdduraes@gmail.com>
2025-12-01 01:56:25 -0300

    Implement end screen logic

--- Assets/Scripts/Level/Level.cs
+++ Assets/Scripts/Level/Level.cs
@@ -6,7 +6,6 @@ namespace Droppy.Level
     public class Level : MonoBehaviour
     {
         public event Action OnStarted = delegate { };
-        public event Action OnFinished = delegate { };
 
         public void StartLevel()
         {
