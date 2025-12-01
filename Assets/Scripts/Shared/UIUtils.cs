2c886da92542b5f96eb504074a2080fd1c9763c9
Filipe Durães <fhdduraes@gmail.com>
2025-12-01 15:38:08 -0300

    Create main menu and droppy fall animation

---
+++ Assets/Scripts/Shared/UIUtils.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Droppy.Shared
+{
+    public static class UIUtils
+    {
+        public static void SetAlpha(this Image image, float alpha)
+        {
+            Color fadeImageColor = image.color;
+            fadeImageColor.a = alpha;
+            image.color = fadeImageColor;
+        }
+    }
+}
