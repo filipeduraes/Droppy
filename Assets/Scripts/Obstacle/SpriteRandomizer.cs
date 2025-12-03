2f6e1b1a50dea057e8db49d2cef536bda982aebd
Filipe Durães <fhdduraes@gmail.com>
2025-12-03 09:53:38 -0300

    Add sprite randomizer and filter animations

---
+++ Assets/Scripts/Obstacle/SpriteRandomizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Droppy.Shared;
+using UnityEngine;
+
+namespace Droppy.Obstacle
+{
+    public class SpriteRandomizer : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private List<Sprite> sprites;
+
+        private void OnEnable()
+        {
+            Sprite randomSprite = sprites.GetRandomElement();
+            spriteRenderer.sprite = randomSprite;
+        }
+    }
+}
